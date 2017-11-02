c4db48da034d24d4093b18a40fec167cb2995c32
Renato <renato145@hotmail.com>
2017-11-01 23:01:26 -0500

    test model works

--- propuesta/iseq2seq/utils.py
+++ propuesta/iseq2seq/utils.py
@@ -25,71 +25,21 @@ def load_anki_dataset(path):
 
     examples_train = loaded['train']
     examples_test = loaded['test']
+    examples_val = loaded['val']
 
     ds_train = data.Dataset(examples_train, [('en', EN), ('es', ES)])
     ds_test = data.Dataset(examples_test, [('en', EN), ('es', ES)])
+    ds_val = data.Dataset(examples_val, [('en', EN), ('es', ES)])
 
-    ES.build_vocab(ds_train.es)
-    EN.build_vocab(ds_train.en)
+    ES.build_vocab(ds_train.es, min_freq=3)
+    EN.build_vocab(ds_train.en, min_freq=3)
 
-    return ds_train, ds_test, ES, EN
+    return ds_train, ds_test, ds_val, ES, EN
 
-def as_minutes(s):
-    m = math.floor(s / 60)
-    s -= m * 60
-    return '%dm %ds' % (m, s)
-
-def time_since(since, percent):
-    now = time.time()
-    s = now - since
-    es = s / (percent)
-    rs = es - s
-    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
-
-
-def sequence_mask(sequence_length, max_len=None):
-    if max_len is None:
-        max_len = sequence_length.data.max()
-    batch_size = sequence_length.size(0)
-    seq_range = torch.range(0, max_len - 1).long()
-    seq_range_expand = seq_range.unsqueeze(0).expand(batch_size, max_len)
-    seq_range_expand = Variable(seq_range_expand)
-    if sequence_length.is_cuda:
-        seq_range_expand = seq_range_expand.cuda()
-    seq_length_expand = (sequence_length.unsqueeze(1)
-                         .expand_as(seq_range_expand))
-    return seq_range_expand < seq_length_expand
-
-
-def masked_cross_entropy(logits, target, length):
-    length = Variable(torch.LongTensor(length)).cuda()
-
-    """
-    Args:
-        logits: A Variable containing a FloatTensor of size
-            (batch, max_len, num_classes) which contains the
-            unnormalized probability for each class.
-        target: A Variable containing a LongTensor of size
-            (batch, max_len) which contains the index of the true
-            class for each corresponding step.
-        length: A Variable containing a LongTensor of size (batch,)
-            which contains the length of each data in a batch.
-    Returns:
-        loss: An average loss value masked by the length.
-    """
-
-    # logits_flat: (batch * max_len, num_classes)
-    logits_flat = logits.view(-1, logits.size(-1))
-    # log_probs_flat: (batch * max_len, num_classes)
-    log_probs_flat = functional.log_softmax(logits_flat)
-    # target_flat: (batch * max_len, 1)
-    target_flat = target.view(-1, 1)
-    # losses_flat: (batch * max_len, 1)
-    losses_flat = -torch.gather(log_probs_flat, dim=1, index=target_flat)
-    # losses: (batch, max_len)
-    losses = losses_flat.view(*target.size())
-    # mask: (batch, max_len)
-    mask = sequence_mask(sequence_length=length, max_len=target.size(1))
-    losses = losses * mask.float()
-    loss = losses.sum() / length.float().sum()
-    return loss
+def write_training_log(file, epoch, tf_ratio, train_loss, test_loss):
+    lines = ''
+    if not os.path.exists(file):
+        lines += 'epoch,teacher_forcing_ratio,train_loss,test_loss\n'
+    lines += f'{epoch},{tf_ratio},{train_loss},{test_loss}\n'
+    with open(file, 'a') as f:
+        f.writelines(lines)
